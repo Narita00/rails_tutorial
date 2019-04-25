e0f2395d9df0957c4dd015d05033e52e06421235
narita <narita@example.com>
2019-04-25 22:54:42 +0900

    End rails tutorial_1

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "hello, world!"
-  end
-
   def goodbye
     render html: "goodbye, world!"
   end
